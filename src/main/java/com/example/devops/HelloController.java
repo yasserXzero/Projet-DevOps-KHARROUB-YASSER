ae62994c07e8dafb415e9b481feb6ea41607b029
yasserXzero <129132890+yasserXzero@users.noreply.github.com>
2026-01-11 14:56:22 +0100

    changing to bon courage

--- src/main/java/com/example/devops/HelloController.java
+++ src/main/java/com/example/devops/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "hello hello we changed the text ";
+        return "Bonjour et bon courage dans votre projet en DevOps";
     }
     @GetMapping("/health")
     public String health() {
